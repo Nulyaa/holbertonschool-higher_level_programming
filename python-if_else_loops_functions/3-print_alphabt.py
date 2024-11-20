d5647a2683065613b1ec930a2aba5e14e248f38c
Nulyaa <nubarmehdiyeva.t@gmail.com>
2024-11-20 16:49:46 +0400

    done

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,5 @@
 for i in range(97, 123):
     if i == ord("q") or i == ord("e"):
         continue
-    else:
-        print("{:c}".format(i), end="")
-        
+    print("{:c}".format(i), end="")
+    

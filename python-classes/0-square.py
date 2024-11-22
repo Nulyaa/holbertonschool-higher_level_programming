b4ebe68abb094a79e63dbaac88762ba253d96e4f
Nulyaa <nubarmehdiyeva.t@gmail.com>
2024-11-22 18:10:44 +0400

    done

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    Squares each element of a matrix (list of lists).
+
+    Args:
+        matrix (list): A 2D list (matrix) where each element is a number.
+
+    Returns:
+        list: A new matrix (2D list) with each element squared.
+    """
     return [[x**2 for x in row] for row in matrix]
